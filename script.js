96924b9ebdaa678cd37d406713d3d7cd58f12e39
Soy_NooV <91479490+soynoov@users.noreply.github.com>
2023-10-03 17:36:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function miFuncion() {
+    document.body.style.backgroundColor = "#E74C3C";
+}
+
+function home() {
+    document.body.style.backgroundColor = "var(--black)"
+}
+
+function prueba() {
+    document.body.style.backgroundColor = "#58D68D"
+}
+
+function maps() {
+    document.body.style.backgroundColor = "#F7DC6F"
+}
