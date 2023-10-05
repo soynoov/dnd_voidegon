863b869a146ba7a2a4c9aedcb1ee8c47e11b5a86
Soy_NooV <91479490+soynoov@users.noreply.github.com>
2023-10-05 16:31:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-function miFuncion() {
-    document.body.style.backgroundColor = "salmon";
-}
+const mainTitle = document.querySelector("#main-title");
 
 function home() {
-    document.body.style.backgroundColor = "var(--black)"
+    document.body.style.backgroundColor = "var(--black)";
+    mainTitle.textContent = "Welcome to Voidegon!";
 }
 
-function prueba() {
-    document.body.style.backgroundColor = "#58D68D"
+function travelDiary() {
+    document.body.style.backgroundColor = "salmon";
+    mainTitle.textContent = "Travel Diary";
+}
+
+function characters() {
+    document.body.style.backgroundColor = "#58D68D";
+    mainTitle.textContent = "Characters";
 }
 
 function maps() {
-    document.body.style.backgroundColor = "#F7DC6F"
+    document.body.style.backgroundColor = "#F7DC6F";
+    mainTitle.textContent = "Maps";
 }
