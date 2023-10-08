fe0b83fb497e86c9c0cd2417709c28f79704dc94
Soy_NooV <heroyi3000@gmail.com>
2023-10-08 19:08:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const mainTitle = document.querySelector("#main-title");
 
 function home() {
-    document.body.style.backgroundColor = "var(--black)";
     mainTitle.textContent = "Welcome to Voidegon!";
 }
 
 function travelDiary() {
-    document.body.style.backgroundColor = "salmon";
     mainTitle.textContent = "Travel Diary";
 }
 
 function characters() {
-    document.body.style.backgroundColor = "#58D68D";
     mainTitle.textContent = "Characters";
 }
 
 function maps() {
-    document.body.style.backgroundColor = "#F7DC6F";
     mainTitle.textContent = "Maps";
 }
